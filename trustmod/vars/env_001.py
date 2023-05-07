71c15d2af9f4ca661c2bf349f585de0effa346c5
jjbingsy <97576913+jjbingsy@users.noreply.github.com>
2023-05-07 19:25:54 -0400

    test llo

--- trustmod/vars/env_001.py
+++ trustmod/vars/env_001.py
@@ -1,7 +1,7 @@
-SIMLINK_DIRECTORY = ''
-IMAGE_DIRECTORY = ''
-MEDIA_DIRECTORIES = ''
-USER_AGENT_GOOGLE = ''
-FILMSOURCES_PATH = ''
-IDOLSDB_PATH = ''
-IDOLS2DB_PATH = ''
+SIMLINK_DIRECTORY = 'C:\\Users\\bing\\Desktop\\link.windows'
+IMAGE_DIRECTORY = 'F:/data/trusti' #'C:\\Users\\bing\\Desktop\\PPO' #'F:/data/trusti' #'C:\\Users\\bing\\Desktop\\trusti' #C:\Users\bing\Desktop\trusti
+MEDIA_DIRECTORIES = ['G:/NewTrust', 'R:/Jav', 'D:/newtrust', 'P:/Jav', 'O:/Jav', 'S:/Jav'] 
+USER_AGENT_GOOGLE = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+FILMSOURCES_PATH = 'filmSources.db'
+IDOLSDB_PATH = 'C:\\Users\\bing\\idolsoo23.db'
+IDOLS2DB_PATH = 'idols9E2E.db'
