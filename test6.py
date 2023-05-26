d943be55f1eba4011a9710ca31d34bc93ab31f34
jjbingsy <97576913+jjbingsy@users.noreply.github.com>
2023-05-26 05:29:03 -0400

    majorttt

--- test6.py
+++ test6.py
@@ -7,26 +7,41 @@ query = """select DISTINCT fi.film_name from idols i join film_idols fi on i.lin
 queryIdols = """select I.name, i.shared_key, i.link from idols i join film_idols fi on i.link = fi.idol_link where fi.film_name = ?;"""
 queryfilmLink = """select source_link from film_sources where film_name = ?;"""
 
+q1 = '''
+select i.shared_key, count(distinct fi.film_name) c from film_idols fi join idols i on fi.idol_link = i.link group by i.shared_key having shared_key not null and c > 1 order by c desc;
+'''
+q2 = '''
+select fi.film_name, count(distinct i.shared_key) c from film_idols fi join idols i on fi.idol_link = i.link group by fi.film_name having c = 1 and i.shared_key = ?;
+'''
 
-conn = sqlite3.connect(IDOLSDB_PATH)
+def getSharedKey():
+    conn = sqlite3.connect(IDOLSDB_PATH)
 
-cursor = conn.cursor()
-cursor.execute(query)
-results = cursor.fetchall()
-for fil in results:
-    film = fil[0]
-    print(film)
-    idols = cursor.execute(queryIdols, (film,)).fetchall()
-    for idol in idols:
-        print(film, idol[0], idol[1], idol[2])
-    i = input("pause: ")
-    if i == "o":
-        cursor.execute (queryfilmLink, (film,))
-        film_link = cursor.fetchall()
-        for link in film_link:
-            webbrowser.open(link[0])
-        i = input("pause: ")
+    cursor = conn.cursor()
+    cursor.execute(q1)
+    results = cursor.fetchall()
+    for i in results:
+        shared_key = i[0]
+        cursor.execute(q2, (shared_key,))
+        results2 = cursor.fetchone()
+        if results2:
+            print (results2[0], shared_key)
+        else:
+            print (shared_key)
+            input("pause: ")
+    conn.close()
 
 
-
-conn.close()
+# for fil in results:
+#     film = fil[0]
+#     print(film)
+#     idols = cursor.execute(queryIdols, (film,)).fetchall()
+#     for idol in idols:
+#         print(film, idol[0], idol[1], idol[2])
+#     i = input("pause: ")
+#     if i == "o":
+#         cursor.execute (queryfilmLink, (film,))
+#         film_link = cursor.fetchall()
+#         for link in film_link:
+#             webbrowser.open(link[0])
+#         i = input("pause: ")
--- test_kivy2.py
+++ test_kivy2.py
@@ -30,7 +30,7 @@ MDScreen:
                         md_bg_color: "#e7e4c0"
                         specific_text_color: "#4a4939"
                         left_action_items:
-                            [['menu', lambda x: nav_drawer.set_state("open")]]
+                            [['menu', lambda x: nav_drawer.set_state("open")], ['menu', lambda x: app.myOp.rc.ii()]]
                     MyRec:
                         id: rec
                         viewclass: 'MyTile'
@@ -52,6 +52,25 @@ MDScreen:
 
             ContentNavigationDrawer:
 '''
+
+class MyOp():
+    def __init__(self, myapp):
+        self.app = myapp
+
+    def myRC(self, myRC):
+        self.rc = myRC
+
+    def click1(self, new_idol):
+        self.rc.jj(new_idol)
+        print (type (self.rc))
+        print ("click1")
+        for i in self.rc.ids:
+            print (type(i))
+
+
+
+
+
 def playme (txt1):
     # print (txt1)
     t = ["C:/Users/bing/Desktop/mpv/mpv.exe", "--fs", "--fs-screen=0", "--loop-playlist" ]
@@ -68,22 +87,54 @@ def playme (txt1):
 
 class MyTile (MDSmartTile):
     texti = StringProperty('')
+    act_option = StringProperty('')
     menu_items = ObjectProperty()
     def on_release(self, *args):
-        thread = threading.Thread(target=playme, args=(self.texti,))
-        thread.start()
-        print (self.texti)
-        return super().on_release(*args)
+        if self.act_option == 'idol':
+            MDApp.get_running_app().myOp.rc.jj(self.texti)
+        elif self.act_option == 'play':
+            thread = threading.Thread(target=playme, args=(self.texti,))
+            thread.start()
+            print (self.texti)
+            return super().on_release(*args)
+        
 
 
 def films_from_idols(idols):
     conn = sqlite3.connect(IDP)
     c = conn.cursor()
-    q1 = f"select distinct fi.film_name from film_idols fi join idols i on fi.idol_link = i.link   where i.shared_key = {idols};"
+    q1 = f"select DISTINCT i.shared_key from idols i join fiLm_idols f on i.link = f.idol_link WHERE film_name = '{idols}';"
     c.execute(q1)
+    results = c.fetchone()
+    q2 = f"select distinct fi.film_name from film_idols fi join idols i on fi.idol_link = i.link   where i.shared_key = {results[0]};"
+    c.execute(q2)
     films = c.fetchall()
     print (IDP)
-    films2 = [{'source': f'{IDD}/{i[0]}.jpg', 'texti' : i[0] } for i in films]
+    films2 = [{'source': f'{IDD}/{i[0]}.jpg', 'texti' : i[0], 'act_option':'play' } for i in films]
+    return films2
+
+def getSharedKey():
+    q1 = '''
+    select i.shared_key, count(distinct fi.film_name) c from film_idols fi join idols i on fi.idol_link = i.link group by i.shared_key having shared_key not null and c > 1 order by c desc;
+    '''
+    q2 = '''
+    select fi.film_name, count(distinct i.shared_key) c from film_idols fi join idols i on fi.idol_link = i.link group by fi.film_name having c = 1 and i.shared_key = ?;
+    '''
+
+    conn = sqlite3.connect(IDP)
+    kk = []
+    cursor = conn.cursor()
+    cursor.execute(q1)
+    results = cursor.fetchall()
+    for i in results:
+        shared_key = i[0]
+        cursor.execute(q2, (shared_key,))
+        results2 = cursor.fetchone()
+        if results2:
+            #print (results2[0], shared_key)
+            kk.append ( results2[0] )
+    conn.close()
+    films2 = [{'source': f'{IDD}/{i}.jpg', 'texti' : i, 'act_option': 'idol' } for i in kk]
     return films2
 
 
@@ -91,16 +142,45 @@ def films_from_idols(idols):
 
 class MyRec(RecycleView):
     def __init__(self, **kwargs):
+        MDApp.get_running_app().myOp.myRC(self)
         super().__init__(**kwargs)
         #self.data = films_from_idols(83, 43) # [{'text': str(x)} for x in range(50)]
-        self.data = films_from_idols(43) # [{'text': str(x)} for x in range(50)]
+        self.data = getSharedKey() # [{'text': str(x)} for x in range(50)]
+
+
+    def jj(self, new_idol):
+        ss = None
+        for i in self.children:
+            print (type(i))
+            ss = i
+            if i:
+                self.data = {'source': f'{IDD}/ADN-195.jpg', 'text' : "ADN-195" }
+                i.clear_widgets()
+                self.data = films_from_idols(new_idol) # [{'text': str(x)} for x in range(50)]
+            # self.ids.rgg.clear_widgets()
+
+    def ii(self):
+        ss = None
+        for i in self.children:
+            print (type(i))
+            ss = i
+            if i:
+                self.data = {'source': f'{IDD}/ADN-195.jpg', 'text' : "ADN-195" }
+                i.clear_widgets()
+                self.data = getSharedKey()
+            # self.ids.rgg.clear_widgets()
+
+
+
 
 class ContentNavigationDrawer(MDBoxLayout):
     pass
 
 
 class Example(MDApp):
+    myOp = ObjectProperty()
     def build(self):
+        self.myOp = MyOp(self)
         self.theme_cls.theme_style = "Dark"
         return Builder.load_string(KV)
 
